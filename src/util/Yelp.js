36500e33c308c5b14c83f7f2fd46ba7afa14f423
Amanda Romig <aromig@live.com>
2020-10-12 13:39:01 -0400

    Functional Ravenous

---
+++ src/util/Yelp.js
@@ -0,0 +1,32 @@
+const apiKey = "THxLFBCwV5PwPdexMR3Pr_RkizBom6mykRQHC7KoZOoyMZYKyZaHO9gVxBr1HWaYplDbiTooWsRmdXcWRlCPWiRZtPpS_F1LCS9EXmqz0Am6SpxhCY3Qs5Hs-YCEX3Yx";
+
+const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (jsonResponse.businesses) {
+               return jsonResponse.businesses.map(business => ({
+                      id: business.id,
+                      imageSrc: business.image_url,
+                      name: business.name,
+                      address: business.location.address1,
+                      city: business.location.city,
+                      state: business.location.state,
+                      zipCode: business.location.zip_code,
+                      category: business.categories[0].title,
+                      rating: business.rating,
+                      reviewCount: business.review_count
+                  })); 
+               }
+            });
+        }
+};
+
+
+
+export default Yelp;
